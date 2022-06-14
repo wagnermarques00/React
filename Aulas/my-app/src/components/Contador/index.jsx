a0a5d3e5a50a45e2265b2eaa44a4a91c6f309fa5
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2022-06-13 21:59:50 -0300

    2022/06/13 update

--- Aulas/my-app/src/components/Contador/index.jsx
+++ Aulas/my-app/src/components/Contador/index.jsx
@@ -3,37 +3,38 @@ import { Container } from "../../Styles";
 import { Botao, MensagemErro, Titulo } from "./Styles";
 
 const Contador = () => {
-  const [numero, setNumero] = useState(0);
-  const [erro, setErro] = useState(false);
+    const [numero, setNumero] = useState(0);
+    const [erro, setErro] = useState(false);
 
-  const aumentarAteDez = () => {
-    if (numero >= 10) {
-      setErro(true);
-      return;
-    } else {
-      setNumero(numero + 1);
-    }
-  };
+    const aumentarAteDez = () => {
+        if (numero >= 10) {
+            setErro(true);
+            return;
+        } else {
+            setNumero(numero + 1);
+        }
+    };
 
-  const zerarContagem = () => {
-    setNumero(0);
-    setErro(false);
-  };
+    const zerarContagem = () => {
+        setNumero(0);
+        setErro(false);
+    };
 
-  return (
-    <Container>
-      <Titulo>Contador</Titulo>
-      {/* PROP: Parâmetros dos componentes React */}
-      {erro && <MensagemErro>Não é possível acrescentar</MensagemErro>}
-      <Botao onClick={() => aumentarAteDez()}>Aumentar</Botao>
-      <Botao onClick={() => zerarContagem()}>Zerar</Botao>
-      <Texto valor={numero} />
-    </Container>
-  );
+    return (
+        <Container>
+            <Titulo>Contador</Titulo>
+            {/* PROP: Parâmetros dos componentes React */}
+            {erro && <MensagemErro>Não é possível acrescentar</MensagemErro>}
+            <Botao onClick={() => aumentarAteDez()}>Aumentar</Botao>
+            <Botao onClick={() => zerarContagem()}>Zerar</Botao>
+            <Texto valor={numero} />
+        </Container>
+    );
 };
 
+//Prop
 const Texto = ({ valor }) => {
-  return <span>{valor}</span>;
+    return <span>{valor}</span>;
 };
 
 export default Contador;
--- Aulas/my-app/src/components/Corpo/index.jsx
+++ Aulas/my-app/src/components/Corpo/index.jsx
@@ -1,11 +1,45 @@
 import Contador from "../Contador";
 import Formulario from "../Formulario";
+import Lista from "../Lista";
+import ListagemProdutos from "../ListagemProdutos";
+import Produto from "../Produto";
 
 function Corpo() {
+    //Simulando como o valor chega de uma API
+    const produtos = [
+        {
+            nome: "Xbox",
+            valor: 10000,
+            imagem: "https://static.nagem.com.br/util/artefatos/produtos/m/n/812031600888847/525324.jpg",
+        },
+        {
+            nome: "Batata doce",
+            valor: 50,
+            imagem: "https://www.proativaalimentos.com.br/image/cache/catalog/img_prod/iStock-496551718[1]-1000x1000.jpg",
+        },
+        {
+            nome: "Bala Halls",
+            valor: 5,
+            imagem: "https://www.drogariaminasbrasil.com.br/media/product/b97/bala-halls-extra-forte-bb7.jpg",
+        },
+    ];
+
     return (
         <div>
             <Contador />
             <Formulario />
+            <Lista>
+                <Lista.Item conteudo="Paraguai" />
+                <Lista.Item conteudo="Uruguai" />
+                <Lista.Item conteudo="Argentina" />
+                <Lista.Item conteudo="Venezuela" />
+            </Lista>
+
+            <ListagemProdutos>
+                {produtos.map((produto) => {
+                    return <Produto nome={produto.nome} valor={produto.valor} imagem={produto.imagem} />;
+                })}
+            </ListagemProdutos>
         </div>
     );
 }
